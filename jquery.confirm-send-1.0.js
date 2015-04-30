a82b55380568c0cd7cb8fb67ade08a846f715683
Luis Luna <luislun82@gmail.com>
2015-04-30 10:49:32 -0500

    Create jquery.confirm-send-1.0.js
    Primer commit de jquery.confirm-send-1.0.js

---
+++ jquery.confirm-send-1.0.js
@@ -0,0 +1,34 @@
+/*!
+ * jQuery confirm-send plugin
+ * Version 1.00 (30-Abr-2015)
+ * @requires jQuery v1.2.3 or later
+ *
+ * Examples at: https://github.com/luislun/confirm-send
+ * GNU GENERAL PUBLIC LICENSE Version 2
+ *
+ */
+
+ $(function() {
+ 	$.widget( "custom.confirmSend", {
+		options: {
+			message: '',
+			confirmResponse: false
+		},
+		_create: function() {
+			this.tagElement = this.element.tagName;
+			this.setEvents();
+		},
+		showMessage: function() {
+			return confirm( this.options.messahge );
+		},
+		setEvents: function(){
+			this.element.click( function() {
+				if ( $( this )[ 0 ].tagName == 'A' ){
+					if( !that.showMessage() ){
+						return false;
+					}
+				}
+			});
+		}
+	});
+});
